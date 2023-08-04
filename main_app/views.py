b624c8ac32ef92e5049db746f01855711d2deb33
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-08-04 14:08:25 -0600

    part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+finches = [
+    {'name': 'Finch 1', 'type': 'House Finch', 'color': 'red and white' , 'age': 2},
+    {'name': 'Finch 2', 'type': 'Brown-Capped Rosy-Finch', 'color': 'brown', 'age': 4 },
+    {'name': 'Finch 3', 'type': 'American Gold Finch', 'color': 'black and yellow', 'age': 5 }
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+def about(request):
+    return render(request,'about.html')
+def finches_index(request):
+    return render(request, 'finches/index.html',{
+        'finches': finches
+    })
