8ce214ee6aaa01a85541a1ddc371a88138d2bb2b
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-08-08 09:30:13 -0600

    part 2 and 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def _str_(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs = {'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-
-finches = [
-    {'name': 'Finch 1', 'type': 'House Finch', 'color': 'red and white' , 'age': 2},
-    {'name': 'Finch 2', 'type': 'Brown-Capped Rosy-Finch', 'color': 'brown', 'age': 4 },
-    {'name': 'Finch 3', 'type': 'American Gold Finch', 'color': 'black and yellow', 'age': 5 }
-]
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -12,6 +8,20 @@ def home(request):
 def about(request):
     return render(request,'about.html')
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html',{
         'finches': finches
     })
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch':finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['type','color','age']
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
