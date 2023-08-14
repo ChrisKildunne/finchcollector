78488a4037cc7511c74166c392dc11165f21bc34
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-08-13 18:01:35 -0600

    Final Commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 MEALS = (
@@ -7,17 +8,30 @@ MEALS = (
     ('L', 'Lunch'),
     ('D', 'Dinner')
 )
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('detail', kwargs = {'finch_id': self.id})
+    def fed_for_today(self):
+     return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 class Feeding(models.Model):
     date = models.DateField('feeding date')
     meal = models.CharField(
@@ -32,4 +46,5 @@ class Feeding(models.Model):
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
     class Meta:
-     ordering = ['-date']
+        ordering = ['-date']
+
